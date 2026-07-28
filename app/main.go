0eb66af3145daf39d84e2de9a1d309e2e5407828
Andrea Fulghesu <fulghesu_a@yahoo.it>
2026-07-28 10:09:56 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,17 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var _ = fmt.Print
 var not_found_msg = ": command not found"
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := reader.ReadString('\n')
 
 	if err == nil {
-		fmt.Printf("%s%s", command, not_found_msg)
+		message := strings.TrimSpace(command)
+		fmt.Printf("%s%s", message, not_found_msg)
 	}
 }
